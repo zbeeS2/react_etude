4334cb1f481ac754153e79f98a0320d83a710d3a
Kim Yongbin <zabee@codebrick.co>
2022-07-24 23:28:29 +0900

    styled component

---
+++ 5. Styled Component/src/App.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Fatehr = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  backgroundColor: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+function App() {
+  return (
+    <Fatehr>
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Box bgColor="tomato"></Box>
+    </Fatehr>
+  );
+}
+export default App;
